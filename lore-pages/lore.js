efbc42e4d3c54938f9449406d2e2f26c3c89da65
JasonPaulino <jasonp0830@gmail.com>
2023-02-27 22:13:38 -0500

    10 second transition into the game

---
+++ lore-pages/lore.js
@@ -0,0 +1,5 @@
+// Import the playerName variable from start-page.js
+import { playerName } from '../start-page/start-page.js';
+
+// Use the playerName variable in your code
+console.log(playerName);
